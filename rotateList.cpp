8a7a1d61962d48e305ebe6ea3dc1fdaf711c4c6b
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-13 21:47:11 +0530

    Right rotate a list

---
+++ rotateList.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode (int x): val(x), next(NULL) {}
+};
+
+ListNode *rotateRight(ListNode* head, int k) {
+    if (head != NULL) {
+        int size = 1;
+        ListNode* tail = head;
+        while (tail->next != NULL) {
+            tail = tail->next;
+            ++size;
+        }
+        tail->next = head;
+
+        k = k%size;
+
+        int i = 0;
+        ListNode* temp = head;
+        for ( ; i<size-k-1; ++i) {
+            temp = temp->next;
+        }
+
+        head = temp->next;
+        temp->next = NULL;
+    }
+
+    return head;
+}
+
+int main() {
+    ListNode head(10);
+    ListNode second(20);
+    ListNode third(30);
+    ListNode fourth(40);
+    ListNode fifth(50);
+    head.next = &second;
+    second.next = &third;
+    third.next = &fourth;
+    fourth.next = &fifth;
+
+    int k;
+    cin >> k;
+    ListNode* newHead = rotateRight(&head, k);
+    ListNode *temp = newHead;
+    while (temp != NULL) {
+        cout << temp->val << endl;
+        temp=temp->next;
+    }
+
+    return 0;
+}
