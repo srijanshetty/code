bb20bb66e81a22644a45baccb38941dcef3378d8
Srijan R Shetty <srijan.shetty@gmail.com>
2014-10-04 10:18:23 +0530

    Program to remove duplicates from a Linked List

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -22,8 +22,44 @@ int removeDuplicates(int A[], int n) {
     return length;
 }
 
+int removeDuplicates2(int A[], int n) {
+    if (n == 0 || n == 1 || n == 2) {
+        return n;
+    }
+
+
+    int B[n];
+    B[0] = A[0];
+    int i = 1, k = 1, prev = 0;
+
+    while (i < n) {
+        if (A[i] == A[prev]) {
+            B[k++] = A[i++];
+
+            while (A[i] == A[prev] && i < n) {
+                ++i;
+            }
+        } else {
+            prev = i;
+            B[k++] = A[i++];
+        }
+    }
+
+    for (i = 0; i < k; ++i) {
+        A[i] = B[i];
+    }
+    return k;
+}
+
 int main() {
     int A[] = {1,1,2};
-    cout << removeDuplicates(A, 3) << endl;
+
+    int length = removeDuplicates2(A, 3);
+    for (int i = 0; i < length; ++i) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
+
