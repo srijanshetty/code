62119b67012c5f3f7475196526891c839d3d579c
Srijan R Shetty <srijan.shetty@gmail.com>
2014-11-12 11:11:10 +0530

    Printlist

--- lib/list.cpp
+++ lib/list.cpp
@@ -5,3 +5,12 @@ struct ListNode {
     ListNode *next;
     ListNode (int x): val(x), next(NULL) {}
 };
+
+void printList(ListNode *head) {
+    ListNode *iterator = head;
+    while (iterator != NULL) {
+        std::cout << iterator->val << " ";
+        iterator = iterator->next;
+    }
+    std::cout << std::endl;
+}
